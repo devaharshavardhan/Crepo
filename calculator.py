e1c2e45ad6f273a4d67ac5e25c10ed4769cbf371
devaharsha1601@gmail.com <devaharsha1601@gmail.com>
2025-04-05 16:20:17 +0530

    add func

---
+++ calculator.py
@@ -0,0 +1,30 @@
+# calculator.py
+
+def add(x, y):
+    return x + y
+
+
+
+if __name__ == "__main__":
+    while True:
+        print("\nSimple Calculator")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Exit")
+
+        choice = input("Enter choice (1-5): ")
+
+        if choice == '5':
+            print("Exiting... Thank you!")
+            break
+
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print("Result:", add(num1, num2))
+        
+        else:
+            print("Invalid input! Please enter a valid option.")
